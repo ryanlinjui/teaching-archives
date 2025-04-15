345dd12357afb458069762353c9ec08d02ade695
盧浩宇 <brian950816@gmail.com>
2025-04-15 22:05:35 +0800

    fix subtract

--- NTNU-GDG/git-tutorial-2/assignments/team1/Calculator/operator.py
+++ NTNU-GDG/git-tutorial-2/assignments/team1/Calculator/operator.py
@@ -8,7 +8,7 @@ def divide(x, y):
         raise ValueError("Cannot divide by zero")
     return x / y
 def subtract(x,y):
-    x - y
+    return x - y
 # ============================================
 # 請在下方區塊新增你們自訂的運算子函數定義
 # 例如小組 A 可新增其它運算子：
